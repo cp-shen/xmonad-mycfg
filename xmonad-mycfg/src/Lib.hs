6466681b84f9d763cc323791f23ac1212680a686
scp <scp@archlinuxscp.xyz>
2022-03-29 03:53:56 +0800

    add basic configs

--- xmonad-mycfg/src/Lib.hs
+++ xmonad-mycfg/src/Lib.hs
@@ -1,10 +1,26 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (entryPoint) where
 
 import XMonad
+import XMonad.Layout.ThreeColumns
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
-someFunc :: IO ()
-someFunc = xmonad def
+entryPoint :: IO ()
+entryPoint = xmonad $ def
+  {
+    modMask = mod4Mask
+  , layoutHook = myLayouts
+  }
+  `additionalKeysP`
+  [
+    ("M-w", spawn "firefox")
+  , ("M-t", spawn "kitty")
+  ]
+
+myLayouts = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeCol nmaster delta ratio
+    tiled = Tall nmaster delta ratio
+    nmaster = 1
+    ratio = 1/2
+    delta = 3/100
