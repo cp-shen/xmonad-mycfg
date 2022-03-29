77fd2a091caf9e16189bddef23819cd2d4689ec7
scp <scp@archlinuxscp.xyz>
2022-03-29 14:57:43 +0800

    add more key bindings

--- xmonad-mycfg/src/Lib.hs
+++ xmonad-mycfg/src/Lib.hs
@@ -4,17 +4,28 @@ import XMonad
 import XMonad.Layout.ThreeColumns
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import qualified XMonad.StackSet as W
+
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Actions.CycleWS
+
 
 entryPoint :: IO ()
 entryPoint = xmonad $ def
   {
     modMask = mod4Mask
   , layoutHook = myLayouts
+  , terminal = "kitty"
   }
   `additionalKeysP`
   [
     ("M-w", spawn "firefox")
   , ("M-t", spawn "kitty")
+  , ("M-e", spawn "emacsclient -c")
+  , ("M-S-s", shellPrompt def) -- %! Move focus to the previous window
+  , ("M-[", prevWS) -- %! Move focus to the previous window
+  , ("M-]", nextWS) -- %! Move focus to the previous window
   ]
 
 myLayouts = tiled ||| Mirror tiled ||| Full ||| threeCol
