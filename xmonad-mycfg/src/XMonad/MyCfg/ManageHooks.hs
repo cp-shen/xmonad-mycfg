7cb3c9e8b0d759b5a970e9bd0ae95b7f2beaa653
scp <scp@archlinuxscp.xyz>
2022-04-01 17:49:36 +0800

    ManageHooks.hs: format code

--- xmonad-mycfg/src/XMonad/MyCfg/ManageHooks.hs
+++ xmonad-mycfg/src/XMonad/MyCfg/ManageHooks.hs
@@ -1,33 +1,30 @@
 -- |
-
 module XMonad.MyCfg.ManageHooks where
 
+import Control.Monad (liftM2)
 import XMonad
 import XMonad.Hooks.ManageHelpers
 import XMonad.MyCfg.Workspaces
-import Control.Monad (liftM2)
 import qualified XMonad.StackSet as W
 
-
 myManageHook :: ManageHook
-myManageHook = composeAll . concat $
-  [
-    --floating
-    [isDialog --> doFloat]
-  , [className =? c --> doFloat | c <- myFloats]
-    --shifting
-  , [(className =? c1 <||> className =? c2) --> f ws | (c1, c2, f, ws) <- shiftToWs ]
-  ]
-  where
-  doShiftAndGo = doF . liftM2 (.) W.greedyView W.shift
-  myFloats = ["Gimp", "gimp", "xmessage", "Xmessage"]
-  shiftToWs =
-    [
-      ("Glances",  "glances",  doShiftAndGo, wsSystem)
-    , ("Mpv",      "mpv",      doShiftAndGo, wsMusic)
-    , ("Alacritty", "alacritty", doShiftAndGo, wsTerminal)
-    , ("Firefox",  "firefox",  doShiftAndGo, wsWebpages)
-    , ("Chromium", "chromium", doShiftAndGo, wsWebpages)
-    , ("Emacs",    "emacs",    doShiftAndGo, wsEmacs)
-    , ("qbittorrent", "qBittorrent", doShiftAndGo, wsDownloads)
+myManageHook =
+  composeAll . concat $
+    [ --floating
+      [isDialog --> doFloat],
+      [className =? c --> doFloat | c <- myFloats],
+      --shifting
+      [(className =? c1 <||> className =? c2) --> f ws | (c1, c2, f, ws) <- shiftToWs]
     ]
+  where
+    doShiftAndGo = doF . liftM2 (.) W.greedyView W.shift
+    myFloats = ["Gimp", "gimp", "xmessage", "Xmessage"]
+    shiftToWs =
+      [ ("Alacritty", "alacritty", doShiftAndGo, wsTerminal),
+        ("Chromium", "chromium",   doShiftAndGo, wsWebpages),
+        ("Emacs", "emacs",         doShiftAndGo, wsEmacs),
+        ("Firefox", "firefox",     doShiftAndGo, wsWebpages),
+        ("Glances", "glances",     doShiftAndGo, wsSystem),
+        ("Mpv", "mpv",             doShift, wsMusic),
+        ("qbittorrent", "qBittorrent", doShiftAndGo, wsDownloads)
+      ]
