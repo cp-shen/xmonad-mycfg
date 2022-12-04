b656c14095b7dde22d2e25ba850d20d16b16e584
scp <scp@archlinuxscp.xyz>
2022-12-04 17:46:30 +0800

    update workspaces

--- xmonad-mycfg/src/XMonad/MyCfg/Workspaces.hs
+++ xmonad-mycfg/src/XMonad/MyCfg/Workspaces.hs
@@ -15,19 +15,19 @@ where
 ws1 = "终端"
 ws2 = "网页"
 ws3 = "代码"
-ws4 = "娱乐"
-ws5 = "下载"
-ws6 = "其他"
-ws7 = "第七"
-ws8 = "第八"
-ws9 = "第九"
-ws10 = "第十"
+ws4 = "计时"
+ws5 = "娱乐"
+ws6 = "下载"
+ws7 = "其他"
+ws8 = "8"
+ws9 = "9"
+ws10 = "10"
 
 wsTerminal = ws1
 wsWebpages = ws2
 wsCode = ws3
-wsEntertain = ws4
-wsDownloads = ws5
+wsEntertain = ws5
+wsDownloads = ws6
 
 myWsList =
-  [ ws1, ws2, ws3, ws4, ws5, ws6]
+  [ ws1, ws2, ws3, ws4, ws5, ws6, ws7]
