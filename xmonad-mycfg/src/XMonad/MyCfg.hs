a5640e749f7acd2219a20621c5ecb644c41a477b
scp <scp@archlinuxscp.xyz>
2022-12-17 23:43:39 +0800

    formatting

--- xmonad-mycfg/src/XMonad/MyCfg.hs
+++ xmonad-mycfg/src/XMonad/MyCfg.hs
@@ -1,19 +1,17 @@
--- |
 module XMonad.MyCfg (entryPoint) where
 
 import qualified Data.Map as M
 import XMonad
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.StatusBar
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.StatusBar
 import qualified XMonad.MyCfg.ColorSchemes.OneDark as Cs
 import XMonad.MyCfg.Keybindings
 import XMonad.MyCfg.Layouts
 import XMonad.MyCfg.ManageHooks
+import XMonad.MyCfg.MouseBindings
 import XMonad.MyCfg.StatusBar
 import XMonad.MyCfg.Workspaces
-import XMonad.MyCfg.MouseBindings
-import qualified XMonad.StackSet as W
 
 entryPoint :: IO ()
 entryPoint =
@@ -24,17 +22,13 @@ myConfig =
     { modMask = mod4Mask,
       terminal = "alacritty",
       workspaces = if null myWsList then map show [1 .. 9 :: Int] else myWsList,
-
       layoutHook = avoidStruts myLayouts,
       manageHook = myManageHook,
       borderWidth = 2,
-
       normalBorderColor = Cs.lowWhite,
       focusedBorderColor = Cs.magenta,
-
       focusFollowsMouse = False,
       clickJustFocuses = True,
-
       keys = myKeys,
       mouseBindings = \c -> M.union (XMonad.mouseBindings def c) (myMouseBindings c)
     }
