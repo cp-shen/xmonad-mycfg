2554765d0a9cb546ff387f668c29e33471433e9c
scp <scp@archlinuxscp.xyz>
2022-12-20 00:07:02 +0800

    add mouse bindings

--- xmonad-mycfg/src/XMonad/MyCfg/MouseBindings.hs
+++ xmonad-mycfg/src/XMonad/MyCfg/MouseBindings.hs
@@ -2,11 +2,14 @@ module XMonad.MyCfg.MouseBindings (myMouseBindings) where
 
 import Data.Map as M
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.StackSet as W
 
 myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings (XConfig {XMonad.modMask = modMask}) =
   M.fromList
     [ ((modMask, button4), \w -> windows W.focusUp),
-      ((modMask, button5), \w -> windows W.focusDown)
+      ((modMask, button5), \w -> windows W.focusDown),
+      ((modMask .|. shiftMask, button4), \w -> moveTo Prev (Not emptyWS)),
+      ((modMask .|. shiftMask, button5), \w -> moveTo Next (Not emptyWS))
     ]
