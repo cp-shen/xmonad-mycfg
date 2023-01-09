ade0f7e2c9e5bf3b008bb51074623e5339d2dbbc
scp <scp@archlinuxscp.xyz>
2023-01-10 02:23:33 +0800

    update mouse bindings

--- xmonad-mycfg/src/XMonad/MyCfg/MouseBindings.hs
+++ xmonad-mycfg/src/XMonad/MyCfg/MouseBindings.hs
@@ -8,8 +8,8 @@ import XMonad.StackSet as W
 myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings (XConfig {XMonad.modMask = modMask}) =
   M.fromList
-    [ ((modMask, button4), \w -> windows W.focusUp),
-      ((modMask, button5), \w -> windows W.focusDown),
-      ((modMask .|. shiftMask, button4), \w -> moveTo Prev (Not emptyWS)),
-      ((modMask .|. shiftMask, button5), \w -> moveTo Next (Not emptyWS))
+    [ ((controlMask .|. shiftMask, button4), \w -> windows W.focusUp),
+      ((controlMask .|. shiftMask, button5), \w -> windows W.focusDown),
+      ((controlMask .|. shiftMask .|. mod1Mask, button4), \w -> moveTo Prev (Not emptyWS)),
+      ((controlMask .|. shiftMask .|. mod1Mask, button5), \w -> moveTo Next (Not emptyWS))
     ]
