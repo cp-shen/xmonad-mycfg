f45c913c7c0394575bbdaa3cef463076fe03763d
scp <scp@archlinuxscp.xyz>
2023-03-29 18:47:05 +0800

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hl = pkgs.haskell.lib.compose;
+        haskellPackages = pkgs.haskellPackages;
+        trv = nixpkgs.lib.trivial;
+        root = ./xmonad-mycfg;
+        project = extraModifiers: returnShellEnv:
+          haskellPackages.developPackage {
+            inherit root;
+            returnShellEnv = returnShellEnv;
+            modifier = (trv.flip trv.pipe) ([
+              hl.enableStaticLibraries
+              hl.justStaticExecutables
+              hl.disableExecutableProfiling
+            ] ++ extraModifiers);
+          };
+      in {
+        packages.default = project [ ] false;
+        devShell = project [
+          (hl.addBuildTools (with haskellPackages; [
+            haskell-language-server
+            cabal-install
+            implicit-hie
+            hlint
+            hpack
+          ]))
+          (hl.overrideCabal (old: {
+            shellHook = (old.shellHook or "") + ''
+              echo "Generating .cabal file from package.yaml using hpack, remember to regenerate if you change package.yaml! Although nix build dont care about it, its mostly for helping haskell-language-server." | ${pkgs.cowsay}/bin/cowsay
+              hpack ${builtins.baseNameOf root}
+
+              export CABAL_CONFIG="$(pwd)/cabal_config"
+            '';
+          }))
+        ] true;
+      }) // {
+        overlay = final: prev: {
+          xmonad-mycfg = self.packages.${final.system}.default;
+        };
+      };
+}
--- xmonad-mycfg/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for xmonad-mycfg
-
-## Unreleased changes
